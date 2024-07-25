f635df527092db5fa7ae5c77b138be793d6e6cac
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-25 23:01:37 +0700

    demo content provider: contacts

--- app/src/main/java/com/example/democomponentsinandroid/contentprovider/ContactsActivity.kt
+++ app/src/main/java/com/example/democomponentsinandroid/contentprovider/ContactsActivity.kt
@@ -1,15 +1,88 @@
 package com.example.democomponentsinandroid.contentprovider
 
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.democomponentsinandroid.R
+import androidx.core.content.ContextCompat
+import androidx.core.database.getStringOrNull
+import com.example.democomponentsinandroid.databinding.ActivityContactsBinding
 
 class ContactsActivity : AppCompatActivity() {
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityContactsBinding.inflate(layoutInflater)
+    }
+
+    private val readContactsPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            // read contacts
+            readContacts()
+        } else {
+            Toast.makeText(this, "Denied Permission", Toast.LENGTH_LONG).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contacts)
+        setContentView(binding.root)
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            readContacts()
+        } else {
+            //  dialog xin quyen
+            readContactsPermissionLauncher.launch(android.Manifest.permission.READ_CONTACTS)
+        }
+    }
+
+    private fun readContacts() {
+        // obj cua content provider
+        val resolver: ContentResolver = this.contentResolver
+
+        val cursor: Cursor? =
+            resolver.query(
+                /* uri = */ ContactsContract.Contacts.CONTENT_URI,
+                /* projection = */ arrayOf(ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME),
+                /* selection = */ "${ContactsContract.Contacts.DISPLAY_NAME} LIKE ?",
+                /* selectionArgs = */arrayOf("abc%"),
+                /* sortOrder = */"${ContactsContract.Contacts.DISPLAY_NAME} ASC"
+            )
+
+        // File("112").bufferedReader().use {  } // sau khi handle file xong se tu dong close
+
+        cursor?.use {
+            // use cursor -> auto close
+            val contacts = mutableListOf<Contact>()
+            val idColumnIndex = cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID)
+            val nameColumnIndex = cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME)
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getStringOrNull(idColumnIndex)
+                val name = cursor.getStringOrNull(nameColumnIndex)
+                if (id != null && name != null) {
+                    contacts.add(Contact(id, name))
+                }
+            }
+
+            if (contacts.isEmpty()) {
+                binding.textView.text = "No contacts found"
+            } else {
+                binding.textView.text = contacts.joinToString("\n") {it.toString()}
+            }
+        }
+        // cursor is closed
+        Log.d("ContactsActivity", "Cursor is closed: ${cursor?.isClosed}")
     }
 }
+
+data class Contact(val id: String, val name: String)
